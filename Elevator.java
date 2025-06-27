132a00386923112bc10a9ee5195365ab8d9f0933
georgeerel <georgeerel1@gmail.com>
2025-06-27 12:56:15 +0300

    Build Elevator and add Constractor

--- Elevator.java
+++ Elevator.java
@@ -1,20 +1,53 @@
 public class Elevator {
-    public boolean isAtTop() {
-        return false ;
+        private String Name;
+        private double Speed;
+        private double Height;
+        private double TargetHeight;
+        private double MaxHeight;
+        private double MinHeight;
+
+    public Elevator(String name, double maxHeight, double minHeigh, double CurrentHeight, double targetheight) {
+        this.Name = name;
+        this.MaxHeight = maxHeight;
+        this.MinHeight = minHeigh;
+        this.Height = CurrentHeight;
+        this.TargetHeight = targetheight;
+        this.Speed = 0;
     }
-    public boolean isAtBottom(){
-        return false;
+    public boolean isAtTop(double height) {
+        if (height >= MaxHeight) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean isAtBottom(double height) {
+        if (height <= MinHeight) {
+            return true;
+        } else {
+            return false;
+        }
     }
     public void goToTop(){
+        while (isAtTop(Height) == false) {
+            Speed=1;
+        }
+
 
     }
     public void goToBottom(){
+        while (isAtBottom(Height)== false){
+            Speed=-1;
+        }
+
 
     }
     public boolean isAtTarget(){
-        return false;
+        if (Height == TargetHeight){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
-
-
-
 }
